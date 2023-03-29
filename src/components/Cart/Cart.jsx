24a27f359c99ed2dc2ad6d6fcb757181eca113e0
Partho Roy <roypartho772@gmail.com>
2023-03-29 22:51:42 +0600

    show data from cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -3,16 +3,22 @@ import './Cart.css'
 const Cart = ({ cart }) => {
     let totalPrice = 0;
     let totalShipping = 0;
+    let quantity = 0;
     for (const product of cart) {
-        totalPrice = totalPrice + product.price;
+        // if (product.quantity === 0) {
+        //     product.quantity = 1;
+        // }
+        // product.quantity = product.quantity || 1;
+        totalPrice = totalPrice + product.price*product.quantity;
         totalShipping = totalShipping + product.shipping;
+        quantity += product.quantity;
     }
     const tax = totalPrice * 7 / 100;
     const grandTotal = totalPrice + totalShipping + tax;
     return (
       <div className="cart">
             <h4 className='order-title'>Order Summary</h4>
-            <p>Selected Items: {cart.length}</p>
+            <p>Selected Items: {quantity}</p>
             <p>Total Price: ${totalPrice}</p>
             <p>Total Shipping Charge: ${totalShipping}</p>
             <p>Tax {tax.toFixed(2)}:</p>
